fd19bc56892eb776d8fd2c0dd2c7103dd4a93aac
Gabriel Petrovay <gabipetrovay@gmail.com>
2012-11-02 16:55:40 +0100

    The initial version of the events library that understands the listen mono configuration options

---
+++ events.js
@@ -0,0 +1,55 @@
+define(["/jquery.js"], function() {
+
+    function addHandlerOnEvent(handler, miid, eventName) {
+
+        var self = this;
+
+        // if the handler is a module function name as string
+        if (typeof handler === "string" && typeof self[handler] === "function") {
+            self.on(eventName, miid, function(data) {
+                self[handler].call(self, data);
+            });
+            return;
+        }
+
+        // else it must be an array of objects
+        if (handler.length) {
+            self.on(eventName, miid, function(data) {
+                for (var i in handler) {
+                    var step = handler[i];
+
+                    // this step is a handler function
+                    if (step.handler) {
+                        if (typeof self[step.handler] === "function") {
+                            self[step.handler].call(self, data);
+                        }
+                        continue;
+                    }
+
+                    // this step is an event emit
+                    if (step.emit) {
+                        self.emit(step.emit, data);
+                        continue;
+                    }
+                }
+            });
+
+            return;
+        }
+    }
+
+    return function(config) {
+
+        var self = this;
+
+        // process only the listen property in the configurations
+        for (var miid in config.listen) {
+
+            var miidEvents = config.listen[miid];
+
+            for (var eventName in miidEvents) {
+                addHandlerOnEvent.call(self, miidEvents[eventName], miid, eventName);
+            }
+        }
+    }
+});
